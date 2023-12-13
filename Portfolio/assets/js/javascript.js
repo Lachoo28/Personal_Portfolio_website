4842efe40d8b0070d9e31d5be8283ad10f77c74a
Lakshan Pulendran <98449126+Lachoo28@users.noreply.github.com>
2023-12-13 22:41:28 +0530

    just java script updated

---
+++ Portfolio/assets/js/javascript.js
@@ -0,0 +1,10 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction(){
+    let menuBtn = document.getElementById("myNavMenu");
+
+    if(menuBtn.className === "nav-menu"){
+      menuBtn.className += " responsive";
+    } else {
+      menuBtn.className = "nav-menu";
+    }
+  }
